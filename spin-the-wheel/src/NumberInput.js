2c0b103bd82a878be1ad7452b8f04728e4578df0
Raeniel Jan Cornelio <raeniel2098@gmail.com>
2023-11-18 07:17:14 -0600

    added component for NumberInput

---
+++ spin-the-wheel/src/NumberInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NumberInput() {
+  return (
+    <div>
+      <input type="number" min="1" max="100" />
+      <button>Add Number</button>
+    </div>
+  );
+}
+
+export default NumberInput;
