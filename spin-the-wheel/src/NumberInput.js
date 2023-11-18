180abe7f5d77bb30ab1381029ba58afc79c6bf0a
Raeniel Jan Cornelio <raeniel2098@gmail.com>
2023-11-18 07:24:57 -0600

    Added code more on NumberInput.js

--- spin-the-wheel/src/NumberInput.js
+++ spin-the-wheel/src/NumberInput.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NumberInput() {
+function NumberInput({ onAddNumber }) {
+  const [number, setNumber] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddNumber(number);
+    setNumber("");
+  };
+
   return (
-    <div>
-      <input type="number" min="1" max="100" />
-      <button>Add Number</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+        min="1"
+        max="100"
+      />
+      <button type="submit">Add Number</button>
+    </form>
   );
 }
 
