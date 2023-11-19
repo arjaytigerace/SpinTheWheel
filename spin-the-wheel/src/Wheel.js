52911b2ea9205b3da0ca36acfba4d4cb31fa2697
Raeniel Jan Cornelio <raeniel2098@gmail.com>
2023-11-19 22:42:28 +0800

    Added UI for Spin Button

--- spin-the-wheel/src/Wheel.js
+++ spin-the-wheel/src/Wheel.js
@@ -1,14 +1,13 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 function Wheel({ numbers, onSpin }) {
   return (
-    <div>
-      <button onClick={onSpin}>Spin</button>
-      <ul>
-        {numbers.map((num, index) => (
-          <li key={index}>{num}</li>
-        ))}
-      </ul>
+    <div style={{ textAlign: "center", margin: "20px 0" }}>
+      <Button variant="contained" color="secondary" onClick={onSpin}>
+        Spin
+      </Button>
+      {/* Visualization of the wheel will go here */}
     </div>
   );
 }
